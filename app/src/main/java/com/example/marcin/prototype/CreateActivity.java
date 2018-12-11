b8633bc32a92aec4456064220dfb361c29b05f6c
Marcin Staszak <marcin.staszak@mindseater.com>
2018-12-11 14:45:13 +0100

    First commit.

---
+++ app/src/main/java/com/example/marcin/prototype/CreateActivity.java
@@ -0,0 +1,21 @@
+package com.example.marcin.prototype;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+
+    public void createTrackNow(View v) {
+        Intent intentApp = new Intent(this, TrackDetails.class);
+
+        this.startActivity(intentApp);
+    }
+}
---
+++ app/src/main/java/com/example/marcin/prototype/TrackDetails.java
@@ -0,0 +1,13 @@
+package com.example.marcin.prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrackDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_details);
+    }
+}
